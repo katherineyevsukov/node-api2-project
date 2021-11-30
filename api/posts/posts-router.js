f5ba7662470ea60517f18d24b44444e9cd34176f
Kat <kyevsukov@gmail.com>
2021-11-30 11:44:54 -0500

    can get post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   Posts.find()
-    .then((users) => {
-      res.status(200).json(users);
+    .then((posts) => {
+      res.status(200).json(posts);
     })
     .catch((err) => {
       res
@@ -14,5 +14,15 @@ router.get("/", (req, res) => {
         .json({ message: "The posts information could not be retrieved" });
     });
 });
+
+router.get("/:id", async (req, res) => {
+    try{
+        const post = await Posts.findById(req.params.id)
+        !post ? res.status(404).json({ message: "The post with the specified ID does not exist" }) :
+        res.status(200).json(post)
+    } catch (err){
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    }
+})
 
 module.exports = router;
