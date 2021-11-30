cb13a419868540056a80924bceb72923c82cb729
Kat <kyevsukov@gmail.com>
2021-11-30 11:17:02 -0500

    index and server files setup

--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('up and running')
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require('./api/server');
+
+server.listen(9000, () => {
+    console.log('server runing on http://localhost:9000')
+})
