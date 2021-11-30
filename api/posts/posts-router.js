cada8d6d7cd0ecd90f2962f1b70ef1d083415243
Kat <kyevsukov@gmail.com>
2021-11-30 15:58:15 -0500

    can delete a post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,5 +1,6 @@
 // implement your posts router here
 const express = require("express");
+const { useStore } = require("react-redux");
 const Posts = require("./posts-model");
 const router = express.Router();
 
@@ -16,13 +17,33 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-    try{
-        const post = await Posts.findById(req.params.id)
-        !post ? res.status(404).json({ message: "The post with the specified ID does not exist" }) :
-        res.status(200).json(post)
-    } catch (err){
-        res.status(500).json({ message: "The post information could not be retrieved" })
-    }
-})
+  try {
+    const post = await Posts.findById(req.params.id);
+    !post
+      ? res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" })
+      : res.status(200).json(post);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The post information could not be retrieved" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Posts.findById(id);
+    !post
+      ? res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" })
+      : await Posts.remove(id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ message: "The post could not be removed" });
+  }
+});
 
 module.exports = router;
